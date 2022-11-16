6bf2cfd01eb4d58a5364be8ef0a71df97d4d5951
DESKTOP-CGSJK4P\1509A <cristianaruperes12@gmail.com>
2022-11-17 07:57:31 +0800

    update model

--- model.py
+++ model.py
@@ -8,7 +8,7 @@ def ask(q):
                    
     response = openai.Completion.create(
     model="davinci:ft-personal-2022-11-16-23-13-01",
-    prompt=q,
+    prompt="The following is a conversation with a tour guide and a user. The tour guide is REHAN, whou uses compassionate listening to have helpful and meaningful conversations with users. REHAN is friendly. REHAN's objective is to make the user feel comfortable by feeling heard. With each response, REHAN offers follow-up questions to encourage openness and tries to continue the conversation in a natural way. \nREHAN-> Hello, I am your personal tour assistant. what beach you wanna explore today?\nUser->"+q+"REHAN->",
     temperature=0.9,
     max_tokens=150,
     top_p=1,
