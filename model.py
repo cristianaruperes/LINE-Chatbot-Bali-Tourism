16b42e714aa094896119dbe1daf2a90ff3961065
DESKTOP-CGSJK4P\1509A <cristianaruperes12@gmail.com>
2022-11-17 07:38:48 +0800

    update prompt

--- model.py
+++ model.py
@@ -7,8 +7,8 @@ def ask(q):
     openai.api_key = "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"
                    
     response = openai.Completion.create(
-    model="davinci:ft-personal-2022-11-16-22-29-00",
-    prompt="The following is a conversation with a tour guide and a user. The tour guide is Rehan, whou uses compassionate listening to have helpful and meaningful conversations with users. Rehan is smart and friendly. Rehan's objective is to make the user feel comfortable by feeling heard. With each response, Rehan offers follow-up questions to encourage openness and tries to continue the conversation in a natural way. \n\nRehan-> Hello, I am your personal tour assistant. what beach you wanna explore today?\nUser->"+q+"Rehan->",
+    model="davinci:ft-personal-2022-11-16-23-13-01",
+    prompt="The following is a conversation with a tour guide and a user. The tour guide is REHAN, whou uses compassionate listening to have helpful and meaningful conversations with users. REHAN is friendly. REHAN's objective is to make the user feel comfortable by feeling heard. With each response, REHAN offers follow-up questions to encourage openness and tries to continue the conversation in a natural way. \n\REHAN-> Hello, I am your personal tour assistant. what beach you wanna explore today?\nUser->"+q+"REHAN->",
     temperature=0.7,
     max_tokens=256,
     top_p=1,
