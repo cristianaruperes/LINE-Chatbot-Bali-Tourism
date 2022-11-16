34aa0f5efc64f3c36bb1ac49136418a216d5b869
DESKTOP-CGSJK4P\1509A <cristianaruperes12@gmail.com>
2022-11-17 02:46:23 +0800

    update model

--- model.py
+++ model.py
@@ -1,13 +1,14 @@
 from linebot import (LineBotApi, WebhookHandler)
 from linebot.exceptions import (InvalidSignatureError)
 from linebot.models import *
+
 def ask(q):
     import openai
     openai.api_key = "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"
                    
     response = openai.Completion.create(
-    model="text-curie-001",
-    prompt=q,
+    model="davinci:ft-personal-2022-11-16-08-38-24",
+    prompt="The following is a conversation with a therapist and a user. The therapist is JOY, who uses compassionate listening to have helpful and meaningful conversations with users. JOY is empathic and friendly. JOY's objective is to make the user feel better by feeling heard. With each response, JOY offers follow-up questions to encourage openness and tries to continue the conversation in a natural way. \n\nJOY-> Hello, I am your personal mental health assistant. What's on your mind today?\nUser->"+q+"JOY->",
     temperature=0.7,
     max_tokens=256,
     top_p=1,
