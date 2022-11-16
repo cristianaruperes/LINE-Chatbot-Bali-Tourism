fe42a4edc9b951d74b3aead09e193e9912633302
DESKTOP-CGSJK4P\1509A <cristianaruperes12@gmail.com>
2022-11-17 01:55:28 +0800

    update response

--- model.py
+++ model.py
@@ -7,14 +7,14 @@ def ask(q):
                    
     response = openai.Completion.create(
     model="davinci:ft-personal-2022-11-16-08-38-24",
-    prompt="The following is a conversation with a therapist and a user. The therapist is JOY, who uses compassionate listening to have helpful and meaningful conversations with users. JOY is empathic and friendly. JOY's objective is to help the user feel better by feeling heard. With each response, JOY offers follow-up questions to encourage openness and tries to continue the conversation in a natural way.",
+    prompt=q,
     temperature=0.7,
     max_tokens=256,
     top_p=1,
     frequency_penalty=0,
     presence_penalty=0,
     # stop=["END"]
-  stop=["\\n"]
+  stop=["\n"]
 )                     
     story = response['choices'][0]['text'] 
     return story
